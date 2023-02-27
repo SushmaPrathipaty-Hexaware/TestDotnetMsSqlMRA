b2a56d9250a76bb980bec39fae0b91ddcd48c32d
SushmaPrathipaty-Hexaware <107030782+SushmaPrathipaty-Hexaware@users.noreply.github.com>
2023-02-27 19:57:20 +0530

    Added change in startup.cs file

--- TestDotnetMsSql/Code/TestDotnetMsSql.Api/Startup.cs
+++ TestDotnetMsSql/Code/TestDotnetMsSql.Api/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using TestDotnetMsSql.Api.Opentelemetry;
+using TestDotnetMsSql.Api.OpenTelemetry;
 using OpenTelemetry.Trace;
 using OpenTelemetry.Exporter;
 
